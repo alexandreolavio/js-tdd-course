41a65fc0cf0515e06d6c4b7b8bedc006b7ee44aa
alexandreolavio <alexandrej.olavio@gmail.com>
2018-07-02 22:13:37 -0300

    criando cenários de testes

---
+++ src/test/caculator.spec.js
@@ -0,0 +1,35 @@
+import { describe, it } from 'mocha';
+
+describe('Calculator', () => {
+  describe('sum', () => {
+    it('deve retornar 4 quando os valores passados forem (2,2)', () => {
+
+    });
+  });
+
+  describe('sub', () => {
+    it('deve retornar 4 quando os valores passados forem (6,2)', () => {
+
+    });
+
+    it('deve retornar -4 quando os valores passados forem (6,10)', () => {
+
+    });
+  });
+
+  describe('mult', () => {
+    it('deve retornar 4 quando os valores passados forem (2,2)', () => {
+
+    });
+  });
+
+  describe('div', () => {
+    it('deve retornar 2 quando os valores passados forem (4,2)', () => {
+
+    });
+
+    it('deve retornar `Não é possível divisão por zero!` quando a divisao for por zero', () => {
+
+    });
+  });
+});
